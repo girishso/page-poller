03aadb697e4d2ae7299c0eccac94ef0c213896d8
Pankaj <pankajso@gmail.com>
2014-10-18 13:01:22 +0530

    Schema modified after db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018020003) do
+ActiveRecord::Schema.define(version: 20141018072825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "provider"
-    t.string   "uid"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
