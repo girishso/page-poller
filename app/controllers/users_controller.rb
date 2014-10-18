7be9a5c1cb943990484e662d732f5a1f8777b34a
Pankaj <pankajso@gmail.com>
2014-10-18 18:41:43 +0530

    edit and update actions added for user show.hmtl.erb renamed to haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
   before_filter :correct_user?, :except => [:index]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def index
     @users = User.all
@@ -10,4 +11,29 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(users_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def users_params
+      params.require(:user).permit(:name, :email, :user_id, :phone)
+    end
 end
