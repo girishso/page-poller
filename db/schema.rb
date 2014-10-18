e5858cec1149350d80a6ed50631133ae08f90075
Pankaj <pankajso@gmail.com>
2014-10-18 21:05:15 +0530

    schema modified after migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018020003) do
+ActiveRecord::Schema.define(version: 20141018151403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141018020003) do
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "guest"
   end
 
 end
