ba67356b6a035081580a4aaa24158f4d927e5ba5
Pankaj <pankajso@gmail.com>
2014-10-18 18:41:05 +0530

    Redirect user to update page after signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     reset_session
     session[:user_id] = user.id
-    redirect_to root_url, :notice => 'Signed in!'
+    redirect_to edit_user_path(user), :notice => 'Signed in!'
   end
 
   def destroy
