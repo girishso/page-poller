459b58bf05be9d21464abe06b7bcecbb03d712fb
Pankaj <pankajso@gmail.com>
2014-10-20 04:35:47 +0530

    Disabled phone update page for time being

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,12 @@ class SessionsController < ApplicationController
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     reset_session
     session[:user_id] = user.id
-    if user.phone.nil?
-      redirect_to edit_user_path(user), :notice => 'Signed in!'
-    else
+    # TODO:: Enable this block when sms functionality added 
+    # if user.phone.nil?
+    #   redirect_to edit_user_path(user), :notice => 'Signed in!'
+    # else
       redirect_to root_url, :notice => 'Signed in!'
-    end
+    # end
   end
 
   def destroy
