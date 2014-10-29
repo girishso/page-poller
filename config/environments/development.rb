a3bcffc72438ec1ff79c183df24aea32450bde96
Girish S <girish.sonawane@gmail.com>
2014-10-29 16:46:18 +0530

    style changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.assets.prefix = "/assets_dev"
+  # config.assets.prefix = "/assets_dev"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
